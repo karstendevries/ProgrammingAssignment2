2b072c5a6afeb05f4454820df0fd71fef0e4cdba
Karsten de Vries <karstendevries@gmail.com>
2015-06-16 21:56:28 +0200

    20150616 - changed cachematrix.R again

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,21 @@
-## Put comments here that give an overall description of what your functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix is a (main) function that stores four functions:
+	## 1 set (changes/stores the matrix stored in the main function)
+	## 2 get (returns the matrix stored in the main function)
+	## 3 setInverseMatrix (changes/stores the inverse matrix in the main function)
+	## 4 getInverseMatrix (returns the inverse matrix stored in the main function)
+## these four functions can be called from the cacheSolve function (see below)
+## x <<- y makes the matrix available in the whole (main) function (same applies with i <<- solve)
+## e.g. function call: 
+	## a <- matrix(c(1,2,3,4),2,2)
+	## w <- makeCacheMatrix(a) 
+## please find additional comments in the function definition as stated below:
 
 makeCacheMatrix <- function(x = matrix()) {
 
 	# initialisation inverse matrix
 	i <- NULL
 
-	# function that sets (cached) matrix and (re)sets (cached) inverse matrix 	
+	# function that sets matrix and (re)sets inverse matrix 	
 	set <- function(y) {
 		x <<- y
 		i <<- NULL
@@ -16,7 +24,7 @@ makeCacheMatrix <- function(x = matrix()) {
 	# function that returns maxtrix
 	get <- function() x
 
-	# function that set inverse matrix
+	# function that sets inverse matrix
 	setInverseMatrix <- function(solve) i <<- solve
 	
 	# function that returns inverse matrix
@@ -26,8 +34,16 @@ makeCacheMatrix <- function(x = matrix()) {
 	list(set=set, get=get, setInverseMatrix=setInverseMatrix, getInverseMatrix=getInverseMatrix)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve is a function that:
+	## 1 gets the inverse matrix (if it exists in memory)
+	## 2 if it exists it will return the inverse matrix and exit the function
+	## 3 else (everything after the first return statement can be seen as a kind of an else condition), get the matrix that is stored during the makeCacheMatrix function call
+	## 4 inverse this matrix 
+	## 5 stores the inverse matrix
+	## 6 returns the inverse matrix
+## e.g. function call:
+	## h <- cacheSolve(w)
+## please find additional comments in the function definition as stated below:
 
 cacheSolve <- function(x = matrix(), ...) {
 
@@ -37,13 +53,13 @@ cacheSolve <- function(x = matrix(), ...) {
 	# check whether the inverse matrix exists
 	if(!is.null(i)) return(i)
 
-	# if no inverse matrix exists, get the one that is given by the makeCacheMatrix function call
+	# if no inverse matrix exists, get the one that is created by the makeCacheMatrix function call
 	matrix <- x$get()
 	
 	# inverse the matrix
 	i <- solve(matrix, ...)
 	
-	# cache the matrix	
+	# store the inverse matrix	
 	x$setInverseMatrix(i)
 	
 	# return inverse matrix
